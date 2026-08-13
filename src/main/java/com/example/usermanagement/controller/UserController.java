dfcaf4ba7452a2a06971af926efcd92ad3203eba
jayasurya-javacodes <suryacse1806@gmail.com>
2026-08-13 13:59:47 +0530

    Changes in contoller and AP

--- src/main/java/com/example/usermanagement/controller/UserController.java
+++ src/main/java/com/example/usermanagement/controller/UserController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class UserController {
 
+
+    // Demo for Git
     @Autowired
     private UserService service;
 
